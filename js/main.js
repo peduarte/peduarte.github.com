27ee0d0b90681cac802557cbac2a5f676f59af29
Pedro Duarte <contact@peduarte.com>
2015-02-03 18:00:14 +0000

    Update

--- js/main.js
+++ js/main.js
@@ -1,21 +1,5 @@
 (function() {
-  var main, project, projectLinks, _i, _len;
 
-  projectLinks = document.querySelectorAll('.Project-item');
-
-  main = document.querySelector('main');
-
-  for (_i = 0, _len = projectLinks.length; _i < _len; _i++) {
-    project = projectLinks[_i];
-    if (project.getAttribute('data-brand-color')) {
-      project.addEventListener('mouseenter', function() {
-        return main.style.backgroundColor = this.getAttribute('data-brand-color');
-      });
-      project.addEventListener('mouseleave', function() {
-        return main.style.backgroundColor = '';
-      });
-    }
-  }
 
 }).call(this);
 
