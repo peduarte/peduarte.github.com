d1b97e861b6198bb5d3d983160c4a356727ac091
Pedro Duarte <contact@peduarte.com>
2015-02-04 19:20:01 +0000

    deploy from ship

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 (function() {
+  var toggler, togglerIsOpened;
 
+  toggler = document.querySelector('.GridToggler');
+
+  togglerIsOpened = false;
+
+  toggler.addEventListener('click', function() {
+    if (!togglerIsOpened) {
+      document.body.classList.add('debug');
+      return togglerIsOpened = true;
+    } else {
+      document.body.classList.remove('debug');
+      return togglerIsOpened = false;
+    }
+  });
 
 }).call(this);
 
